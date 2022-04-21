cefd53de37d53c538fb5ad0c39a98230322dcd15
Clément NICOLAS <clement.nicolas@mixity.co>
2022-04-21 15:50:54 +0200

    install fastapi admin

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from fastapi_admin import app as admin_app
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
@@ -22,3 +23,4 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.mount("/admin", admin_app)
